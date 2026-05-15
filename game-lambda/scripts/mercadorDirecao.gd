3030a0aef8095b9826f1513b6aa28b931c2060ba
Lucas Cavalcante <l.cavalcante@aluno.ifsp.edu.br>
2026-05-14 22:19:22 -0300

    sprites + script mercador
    sprites do fundo e scrip que faz o mercador sempre olhar pro player

--- game-lambda/scripts/mercadorDirecao.gd
+++ game-lambda/scripts/mercadorDirecao.gd
@@ -2,4 +2,21 @@ extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	anim.flip_h = 1
+	pass
+var player: Node2D
+
+func get_player():
+	var nodes = get_tree().get_nodes_in_group("Player")
+	if nodes.size() == 0:
+		push_error("Player não foi encontrado!")
+		return
+			
+	player = nodes[0]
+	
+func _physics_process(_delta: float) -> void:
+	get_player()
+	if player:
+		if player.position < position:
+			anim.flip_h = 1
+		else:
+			anim.flip_h = 0
