d136047e84503a96fdc73179268e2b38abc9d8fe
Nicolas Belisário <nick2007369@gmail.com>
2026-05-20 17:57:16 -0300

    Nicolas 20/05
    Criação do menu inicial e posição do player ao mudar de fase arrumada

--- game-lambda/scripts/level_end.gd
+++ game-lambda/scripts/level_end.gd
@@ -6,4 +6,5 @@ func _on_body_entered(_body: Node2D) -> void:
 	call_deferred("load_next_scene")
 
 func load_next_scene():
+	Global.from_world = get_parent().name
 	get_tree().change_scene_to_file("res://scene/" + next_level + ".tscn")
