a8d3c2bb47328e983712a28fcb8f13e7645b1092
Nicolas Belisário <nick2007369@gmail.com>
2026-06-01 17:45:51 -0300

    Sistema de diálogo
    Por enquanto ele está funcionando no script do level, quero ver se consigo deixar ele no npc ou em algum outro lugar

--- game-lambda/scripts/mercador.gd
+++ game-lambda/scripts/mercador.gd
@@ -1,10 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
 const _DIALOG_SCREEN: PackedScene = preload("res://scene/dialogo_screen.tscn")
 var _dialog_data: Dictionary = {
 	0: {
 		"faceset": "res://sprites/faceset/mercador.png",
-		"dialog": "Ola, seja bem vindo!",
+		"dialog": "Olá, seja bem vindo!",
 		"title": "Mercador"
 	},
 	1: {
@@ -34,17 +34,13 @@ var _dialog_data: Dictionary = {
 var _can_interact: bool = false
 
 func _ready() -> void:
-	MusicaGlobal.tocar_musica("res://sounds/music/shop.mp3")
-	if Global.from_world != null:
-		$Player.global_position = get_node(Global.from_world + "Pos").global_position
-		
-	 #Conecta os sinais da Area2D via código (Padrão do Godot 4)
+	# Conecta os sinais da Area2D via código (Padrão do Godot 4)
 	_dialog_area.body_entered.connect(_on_dialog_area_body_entered)
 	_dialog_area.body_exited.connect(_on_dialog_area_body_exited)
 
 func _process(delta: float) -> void:
 	# Verifica se pode interagir E se o botão foi pressionado
-	if _can_interact and Input.is_action_just_pressed("ui_accept"):
+	if _can_interact and Input.is_action_just_pressed("ui_select"):
 		
 		# Verifica se já não existe um diálogo na tela para evitar sobreposição
 		if _hud.get_child_count() == 0: 
@@ -55,32 +51,21 @@ func _process(delta: float) -> void:
 
 
 # Acionado quando algum corpo entra na Area2D
-func _on_dialog_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"): 
-		_can_interact = true
-
-# Acionado quando algum corpo sai da Area2D
-func _on_dialog_area_body_exited(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		_can_interact = false
-
-
 #func _on_dialog_area_body_entered(body: Node2D) -> void:
 	#print("Alguém entrou na área: ", body.name)
-	#pass # Replace with function body.
+	#if body.is_in_group("Player"): 
+		#_can_interact = true
 #
-#
+## Acionado quando algum corpo sai da Area2D
 #func _on_dialog_area_body_exited(body: Node2D) -> void:
-	#pass # Replace with function body.
+	#if body.is_in_group("Player"):
+		#_can_interact = false
 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#MusicaGlobal.tocar_musica("res://sounds/music/shop.mp3")
-	#if Global.from_world != null:
-		#$Player.global_position = get_node(Global.from_world + "Pos").global_position
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta: float) -> void:
-	#pass
+func _on_dialog_area_body_entered(body: Node2D) -> void:
+	print("Alguém entrou na área: ", body.name)
+	pass # Replace with function body.
+
+
+func _on_dialog_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
